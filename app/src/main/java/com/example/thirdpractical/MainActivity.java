407c6e844192cc93eba82348f15f324c7406d453
ivissivi <ivis31121999@gmail.com>
2021-12-07 14:41:40 +0200

    Added buttons and second activity

--- app/src/main/java/com/example/thirdpractical/MainActivity.java
+++ app/src/main/java/com/example/thirdpractical/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.thirdpractical;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnSecondActivity = findViewById(R.id.btnSecondActivity);
+        btnSecondActivity.setOnClickListener(v -> {
+            Intent activity2Intent = new Intent(getApplicationContext(), SecondActivity.class);
+            startActivity(activity2Intent);
+        });
     }
 }
