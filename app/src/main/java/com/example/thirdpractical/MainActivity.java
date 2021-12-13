fd8ecf746e59960914dd2241774260c9bed75c3d
ivissivi <ivis31121999@gmail.com>
2021-12-13 13:52:04 +0200

    Dropdown menu added

--- app/src/main/java/com/example/thirdpractical/MainActivity.java
+++ app/src/main/java/com/example/thirdpractical/MainActivity.java
@@ -3,9 +3,14 @@ package com.example.thirdpractical;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +20,22 @@ public class MainActivity extends AppCompatActivity {
             Intent activity2Intent = new Intent(getApplicationContext(), SecondActivity.class);
             startActivity(activity2Intent);
         });
+
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.modes, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
