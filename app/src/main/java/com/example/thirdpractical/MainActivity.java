aa51b3d891ff873e68f5a0dec7bcba2bbd8815ad
KarlisPurins <karlis.purins.1999@gmail.com>
2021-12-13 19:12:35 +0200

    Save button calls onClick

--- app/src/main/java/com/example/thirdpractical/MainActivity.java
+++ app/src/main/java/com/example/thirdpractical/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.thirdpractical;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
@@ -8,19 +10,30 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, View.OnClickListener {
+
+    private final String MyPREFERENCES = "preferences";
+    SharedPreferences sharedpreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btnSecondActivity = findViewById(R.id.btnSecondActivity);
+        Button btnSave = (Button) findViewById(R.id.btnSave);
         btnSecondActivity.setOnClickListener(v -> {
             Intent activity2Intent = new Intent(getApplicationContext(), SecondActivity.class);
             startActivity(activity2Intent);
         });
+        btnSave.setOnClickListener(this); //OnClickListener for Save button
+        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+
+
 
         Spinner spinner = findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.modes, android.R.layout.simple_spinner_item);
@@ -28,6 +41,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
     }
+
+
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -41,6 +56,28 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "Hello toast!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        switch (view.getId()) {
+            case R.id.btnSave:
+                EditText editText = findViewById(R.id.editTextTextPersonName);
+                String savedText = editText.getText().toString();
+                toast.setText(savedText);
+                toast.show();
+                sharedpreferences = getSharedPreferences(MyPREFERENCES, context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+                editor.putString("inputText", savedText);
+                editor.commit();
+                break;
+        }
+
 
     }
 }
